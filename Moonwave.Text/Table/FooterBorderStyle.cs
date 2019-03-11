1b4b00e6ee56bc25e8ec07227cd2d2d61a19878e
Shane Johnson <36738000+CARVERD12@users.noreply.github.com>
2019-03-11 17:07:19 -0400

    Add project files.

---
+++ Moonwave.Text/Table/FooterBorderStyle.cs
@@ -0,0 +1,26 @@
+﻿// (c) 2019 Moonwave Interactive. All rights reserved under MIT License.
+
+namespace Moonwave.Text
+{
+    /// <summary>
+    /// Specifies constants that define the border style of the footer.
+    /// </summary>
+    public enum FooterBorderStyle
+    {
+        /// <summary>
+        /// Disables viewing of the footers border.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Displays a single line divider under the footer.
+        /// </summary>
+        Single = 1,
+
+        /// <summary>
+        /// Displays a single line divider on the top and bottom of the footer, only if
+        /// the footer contains statistics, otherwise a single line divider under the footer.
+        /// </summary>
+        Dual = 2
+    }
+}
